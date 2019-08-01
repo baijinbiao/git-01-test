e416c8ba99df6d947bb79aba7eddedc527f9d80e
923031698@qq.com <923031698@qq.com>
2019-08-01 19:12:03 +0800

    init git-01-te

--- src/main/com.bjpowernode.git/Test.java
+++ src/main/com.bjpowernode.git/Test.java
@@ -3,5 +3,6 @@ package com.bjpowernode.git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("@!31231");
+        System.out.println("123123123");
     }
 }
