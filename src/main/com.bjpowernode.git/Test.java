25769b41fe8479626e8a19822d58075df04c00a5
923031698@qq.com <923031698@qq.com>
2019-08-01 20:11:07 +0800

    1

--- src/main/com.bjpowernode.git/Test.java
+++ src/main/com.bjpowernode.git/Test.java
@@ -3,11 +3,5 @@ package com.bjpowernode.git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
-      
-
-      
-
     }
 }
